319ba37ed16fd9fac314349346a5b3c0276d77ff
Wedson Quintanilha da Silva <wedson.silva@oobj.com.br>
2023-04-28 15:25:20 -0300

    add security

--- v1/index.js
+++ v1/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(function (req, res, next) {
+  res.setHeader('Strict-Transport-Security', "max-age=31536000; includeSubDomains always")
+  res.setHeader('X-Frame-Options', "SAMEORIGIN")
+  res.setHeader('X-Content-Type-Options', "nosniff")
+  res.setHeader('X-XSS-Protection', "1; mode=block")
+  res.setHeader('Referrer-Policy', "strict-origin")
+  res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'");
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
